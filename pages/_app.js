15a0003e37ec6610a057d85512bcd66e5eb45630
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-14 09:24:28 +0200

    FIX: slug page was refactored to client-side rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SWRConfig } from "swr";
 
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
@@ -11,7 +12,15 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="NextJS Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          refreshInterval: 3000,
+          fetcher: (resource, init) =>
+            fetch(resource, init).then((res) => res.json()),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
     </Layout>
   );
 }
