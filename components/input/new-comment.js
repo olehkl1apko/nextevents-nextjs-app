a2ad4c6282b456838e2e851d95eb6e3038a91acf
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-14 09:48:41 +0200

    ADD: newsletter signup

--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -5,18 +5,22 @@ function NewComment(props) {
   const [isInvalid, setIsInvalid] = useState(false);
 
   const emailInputRef = useRef();
+  const nameInputRef = useRef();
   const commentInputRef = useRef();
 
   function sendCommentHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
+    const enteredName = nameInputRef.current.value;
     const enteredComment = commentInputRef.current.value;
 
     if (
       !enteredEmail ||
       enteredEmail.trim() === "" ||
       !enteredEmail.includes("@") ||
+      !enteredName ||
+      enteredName.trim() === "" ||
       !enteredComment ||
       enteredComment.trim() === ""
     ) {
@@ -26,6 +30,7 @@ function NewComment(props) {
 
     props.onAddComment({
       email: enteredEmail,
+      name: enteredName,
       text: enteredComment,
     });
   }
@@ -35,16 +40,16 @@ function NewComment(props) {
       <div className={classes.row}>
         <div className={classes.control}>
           <label htmlFor="email">Your email</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="name">Your name</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" ref={nameInputRef} />
         </div>
       </div>
       <div className={classes.control}>
         <label htmlFor="comment">Your comment</label>
-        <textarea id="comment" rows="5"></textarea>
+        <textarea id="comment" rows="5" ref={commentInputRef}></textarea>
       </div>
       {isInvalid && <p>Please enter a valid email address and comment!</p>}
       <button>Submit</button>
